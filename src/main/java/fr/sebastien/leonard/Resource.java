055e843c3d0d3497acbc475f95fd4d25d4bd6b00
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-05-15 10:10:03 +0200

    ...

--- src/main/java/fr/sebastien/leonard/Resource.java
+++ src/main/java/fr/sebastien/leonard/Resource.java
@@ -22,17 +22,11 @@ public class Resource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        String text = "Hello, World! " + client.target(System.getenv("URI"));
-
         WebTarget target = client.target(System.getenv("URI"));
         Invocation.Builder builder = target.request();
-        List<Book> response = builder.get(List.class);
-
-        for(Book book : response) {
-            System.out.println(book.getId() + " " + book.getTitle());
-        }
-
-        return text;
+        String response = builder.get(String.class);
+        
+        return response;
     }
 
 }
