d433762aa97e9ddf7b26e681aeb1beb570bc337a
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-05-16 14:03:31 +0200

    ...

--- src/main/java/fr/sebastien/leonard/Resource.java
+++ src/main/java/fr/sebastien/leonard/Resource.java
@@ -5,10 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.*;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
@@ -27,7 +24,9 @@ public class Resource {
                 .queryParam("idToAdd", id)
                 .queryParam("numberToAdd", number);
 
-        return target.request().buildPut(null).invoke();
+        //Response response = target.request().buildPut(null).invoke();
+
+        return Response.status(200).entity("Hello !").build();
     }
 
 }
