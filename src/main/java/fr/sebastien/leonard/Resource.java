6c1c810a0c845ee0422e80d500bf4d696a8e7ed1
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-05-16 13:22:31 +0200

    ...

--- src/main/java/fr/sebastien/leonard/Resource.java
+++ src/main/java/fr/sebastien/leonard/Resource.java
@@ -5,54 +5,18 @@ import fr.sebastien.leonard.model.Book;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.client.*;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Path("/")
 public class Resource {
-
-    private Client client = ClientBuilder.newClient();
-
+    
     @PUT
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String text = "A";
-
-        try {
-            if(request.getParameter("idToAdd") == null || request.getParameter("numberToAdd") == null) {
-                return text;//Response.status(400).entity("You must give the idToAdd and numberToAdd in parameters of the request").build();
-            }
-
-            System.out.println("B");
-            text += "B";
-
-            Long id = Long.parseLong(request.getParameter("idToAdd"));
-            Long add = Long.parseLong(request.getParameter("numberToAdd"));
-
-            WebTarget target = client.target(System.getenv("URI"))
-                    .path("/")
-                    .queryParam("idToAdd", id)
-                    .queryParam("numberToAdd", add);
-
-            System.out.println("C");
-            text += "C";
-
-            Invocation.Builder builder = target.request();
-            return text;
-        } catch (Exception ex) {
-
-            text+= "D";
-
-            return text;//Response.status(400).entity("You must give the idToId, and numberToId in parameters of the request").build();
-        }
-
+    public Response execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        return Response.status(200).entity("A").build();
     }
 
 }
