557aedc6866bf958d266000277603e3842b8c6aa
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-05-16 13:44:58 +0200

    ...

--- src/main/java/fr/sebastien/leonard/Resource.java
+++ src/main/java/fr/sebastien/leonard/Resource.java
@@ -1,15 +1,18 @@
 package fr.sebastien.leonard;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
 @Path("/")
 public class Resource {
 
     @PUT
-    public Response onPut() {
-        return Response.status(200).entity("Hello, World!").build();
+    public Response onPut(HttpServletRequest request) {
+
+        return Response.status(200).entity("Hello, World! " + request.getServletPath()).build();
     }
 
 }
