f38550858f48d5ed93550663d9465605d56d4875
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-05-16 13:31:16 +0200

    ...

--- src/main/java/fr/sebastien/leonard/Resource.java
+++ src/main/java/fr/sebastien/leonard/Resource.java
@@ -1,22 +1,14 @@
 package fr.sebastien.leonard;
 
-import com.google.gson.Gson;
-import fr.sebastien.leonard.model.Book;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.client.*;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 
-@Path("/hello")
+@Path("/")
 public class Resource {
 
-    @GET
-    public Response onGet() {
+    @PUT
+    public Response onPut() {
         return Response.status(200).entity("Hello, World!").build();
     }
 
