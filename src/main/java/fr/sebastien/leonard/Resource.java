856b03b8eab29b8ec0d02bd51042afecc7713b94
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-05-16 13:59:58 +0200

    ...

--- src/main/java/fr/sebastien/leonard/Resource.java
+++ src/main/java/fr/sebastien/leonard/Resource.java
@@ -5,18 +5,29 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
 @Path("/")
 public class Resource {
 
-    @GET
-    public Response onGet(
+    private Client client = ClientBuilder.newClient();
+
+    @PUT
+    public Response onPut(
         @QueryParam("idToAdd") String id,
         @QueryParam("numberToAdd") Integer number
     ) {
-        return Response.status(200).entity("Hello, World!" + id + " -> " + number).build();
+        WebTarget target = client
+                .target(System.getenv("URI"))
+                .queryParam("idToAdd", id)
+                .queryParam("numberToAdd", number);
+
+        return target.request().buildPut(null).invoke();
     }
 
 }
